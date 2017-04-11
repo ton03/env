14a0f4841eeb6b7682adfb1d39bb77987c975d4d
Antonn Esquivel <antonn.esquivel@iress.com.au>
2017-04-11 14:52:06 +1000

    Moved aliases and functions to respective files

---
+++ aliases.sh
@@ -0,0 +1,20 @@
+## General
+alias ll="ls -l"
+alias lal="ls -al"
+
+## Git
+alias gits="git status"
+alias gitb="git branch"
+alias gitcm="git commit"
+alias gitcma="git commit -a"
+alias gitcmam="git commit -a -m"
+alias gitcmaa="git commit -a --amend"
+alias gitcmaam="git commit -a --amend -m"
+alias gitco="git checkout"
+alias gitcom="git checkout master"
+alias gitcob="git checkout --track origin/master -b"
+alias gitd="git diff"
+alias gitpr="git pull -r"
+alias gitprom="git pull -r origin master"
+alias gitrd="git review --download"
+alias gitrf="git review --finish"
---
+++ functions.sh
@@ -0,0 +1,57 @@
+## Resize an image using 'sips'
+function resize_image {
+    input="$1"
+    output=''
+    width=0
+    height=0
+    v=false
+
+
+    ## Usage and getopts
+    usage () {
+        echo "Usage: resize_image input.png [-o <output.png>] [-w <width in px>] [-h <height in px>]"
+    }
+
+    OPTIND=2
+    while getopts 'o:w:h:v' flag; do
+        case "$flag" in
+            o) output="$OPTARG" ;;
+            w) width="$OPTARG" ;;
+            h) height="$OPTARG" ;;
+            v) v=true ;;
+            *) usage; return 1 ;;
+        esac
+    done
+
+
+    ## Checkers
+    if [ -z "${output}" ]; then
+        $v && echo "No output file, will overwrite '${input}'"
+        output=$input
+    fi
+
+    if [ $width -eq 0 ] || [ $height -eq 0 ]; then
+        echo "Invalid values for width and/or height (must be > 0):"
+        echo "  width:  ${width}"
+        echo "  height: ${height}"
+        return 1
+    fi
+
+
+    ## Let's do this
+    if [ -f $input ]; then
+        echo "Resizing '${input}'"
+
+        if [ "${input}" != "${output}" ]; then
+            $v && echo "  Copying to '${output}'' ..."
+            cp "${input}" "${output}"
+        fi
+
+        sips "${output}" --resampleHeightWidth $height $width
+    else
+        echo "File '$input' does not exist!"
+        return 1
+    fi
+
+    echo "Done!"
+}
