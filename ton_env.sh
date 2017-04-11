092aa30999e8ed3379fb911f3ec5a6f6b33cc395
Antonn Esquivel <antonn.esquivel@iress.com.au>
2017-04-11 15:09:42 +1000

    scriptdir for source so it can be sourced from anywhere

--- ton_env.sh
+++ ton_env.sh
@@ -1,7 +1,7 @@
 ## Ton's environment
-
-source aliases.sh
-source functions.sh
+scriptdir=`dirname "$BASH_SOURCE"`
+source "${scriptdir}/aliases.sh"
+source "${scriptdir}/functions.sh"
 
 ## Pretty colors
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
