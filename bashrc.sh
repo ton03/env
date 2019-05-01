b915c5fd99a4215bc7394a2b673e38ace766533d
Ton <esquivel.antonn@gmail.com>
2019-05-01 12:01:57 +1000

    Updated bashrc

--- bashrc.sh
+++ bashrc.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+DIR=`dirname ${BASH_SOURCE[0]}`
 
 ## Ton's environment
-scriptdir=`dirname "$0"`
-source "${scriptdir}/aliases.sh"
-source "${scriptdir}/functions.sh"
+source "${DIR}/aliases.sh"
+source "${DIR}/functions.sh"
 
-## Pretty colors
-# export PS1="\[\033[33;1m\]\w\[\033[m\] \[\033[32m\]\$(parse_git_branch)\[\033[m\]\n\$ "
-# export CLICOLOR=1
-# export LSCOLORS=ExFxBxDxCxegedabagacad
+## Wow much colors
+export PS1="\[\033[33;1m\]\w\[\033[m\] \[\033[32m\]\$(parse_git_branch)\[\033[m\]\n\$ "
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
